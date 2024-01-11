3ad6ffca2a0fbc2ce42e6edb267babc216042647
boburastonov <boburastonov33@gmail.com>
2024-01-11 23:02:15 +0500

    add winner function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { TicTacToe } from './Components/TicTacToe/TicTacToe';
 
 function App() {
   return (
     <div className="App">
-
+      <TicTacToe />
     </div>
   );
 }
